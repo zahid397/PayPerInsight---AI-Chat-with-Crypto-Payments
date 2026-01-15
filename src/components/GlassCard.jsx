16a7219cbec6365834549d9c6339f21d1abc0baa
zahid397 <zh05698@gmail.com>
2026-01-15 07:51:36 +0600

    Delete src directory

--- src/components/GlassCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function GlassCard({ children, className = '' }) {
-  return (
-    <div className={`glass-panel rounded-2xl ${className}`}>
-      {children}
-    </div>
-  );
-}
